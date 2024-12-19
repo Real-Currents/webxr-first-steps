1812234638f6107f608f50d173329ddeafb4512f
John H <john@real-currents.com>
2024-12-19 18:55:29 -0500

    check it out

--- src/main.js
+++ src/main.js
@@ -9,7 +9,9 @@ import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
 import { VRButton } from "three/addons/webxr/VRButton.js";
 import { XRControllerModelFactory } from "three/addons/webxr/XRControllerModelFactory.js";
 
-import geometry from "./geometry";
+import boxGeometry from "./geometry/boxGeometry";
+import bulletGeometry from "./geometry/bulletGeometry";
+import planeGeometry from "./geometry/planeGeometry";
 import material from "./material";
 
 const controllerModelFactory = new XRControllerModelFactory();
@@ -223,34 +225,15 @@ async function setupScene (camera, controllers, player) {
     camera.position.y = 1;
 
     // Floor
-    const floorGeometry = new THREE.PlaneGeometry(6, 6);
-    const floorMaterial = new THREE.MeshBasicMaterial({color: 'white'});
-    const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    const floor = new THREE.Mesh(planeGeometry, material);
 
     floor.rotateX(-Math.PI / 2);
 
     scene.add(floor);
 
-    // Cone
-    const coneGeometry = new THREE.ConeGeometry(0.6, 1.5);
-    const coneMaterial = new THREE.MeshBasicMaterial({color: 'purple'});
-    const cone = new THREE.Mesh(coneGeometry, coneMaterial);
-
-    cone.position.set(0.4, 0.75, -1.5);
-
-    scene.add(cone);
-
-    // Cube
-    const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-    const cubeMaterial = new THREE.MeshBasicMaterial({color: 'orange'});
-    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    scene.add(cube);
-    cube.position.set(-0.8, 0.5, -1.5);
-    cube.rotateY(Math.PI / 4);
-
     // Rotating Cube
     const rotatingMesh = (controllers.left === null) ?
-        new THREE.Mesh(geometry, material) :
+        new THREE.Mesh(boxGeometry, material) :
         // ... or Controller mesh
         controllerModelFactory.createControllerModel(controllers.left.gripSpace);
 
@@ -268,29 +251,15 @@ async function setupScene (camera, controllers, player) {
 
     scene.add(rotatingMesh);
 
-    // Sphere
-    const sphereGeometry = new THREE.SphereGeometry(0.4);
-    const sphereMaterial = new THREE.MeshBasicMaterial({color: 'red'});
-    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-
-    sphere.position.set(0.6, 0.4, -0.5);
-    sphere.scale.set(1.2, 1.2, 1.2);
-
-    scene.add(sphere);
-
-    // Power Ball
-    const bulletGeometry = new THREE.SphereGeometry(0.02);
-    const bulletMaterial = new THREE.MeshStandardMaterial({color: 'gray'});
-    const bulletPrototype = new THREE.Mesh(bulletGeometry, bulletMaterial);
-
     return function () {
         if (controllers.hasOwnProperty("right") && controllers.right !== null) {
 
             const { gamepad, raySpace } = controllers.right;
 
+            // Power Ball
             if (gamepad.getButtonClick(XR_BUTTONS.TRIGGER)) {
                 console.log("Trigger on right controller was activated:", XR_BUTTONS.TRIGGER, gamepad);
-                const bullet = bulletPrototype.clone();
+                const bullet = new THREE.Mesh(bulletGeometry, material);
                 scene.add(bullet);
                 raySpace.getWorldPosition(bullet.position);
                 raySpace.getWorldQuaternion(bullet.quaternion);
